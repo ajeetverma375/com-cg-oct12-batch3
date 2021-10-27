aef4394badfba749b62fabe23a034ecd10f05e04
ajeetverma375 <ajeetverma375@gmail.com>
2021-10-27 18:30:30 +0530

    new

--- src/main/java/com/cg/oct12/batch3/day8/Interface/LambdaExpressComparator.java
+++ src/main/java/com/cg/oct12/batch3/day8/Interface/LambdaExpressComparator.java
@@ -2,21 +2,25 @@ package com.cg.oct12.batch3.day8.Interface;
 import java.util.ArrayList;  
 import java.util.Collections;  
 import java.util.List;  
-class Product{  
+class Product
+{  
     int id;  
     String name;  
     float price;  
-    public Product(int id, String name, float price) {  
+    public Product(int id, String name, float price) 
+    {  
         super();  
         this.id = id;  
         this.name = name;  
         this.price = price;  
     }  
 }  
-public class LambdaExpressComparator{  
-    public static void main(String[] args) {  
+public class LambdaExpressComparator
+{  
+    public static void main(String[] args)
+    {  
         List<Product> list=new ArrayList<Product>();  
-          
+       
         //Adding Products  
         list.add(new Product(1,"HP Laptop",25000f));  
         list.add(new Product(3,"Keyboard",300f));  
@@ -25,12 +29,12 @@ public class LambdaExpressComparator{
         System.out.println("Sorting on the basis of name...");  
   
         // implementing lambda expression  
-        Collections.sort(list,(p1,p2)->{  
-        return p1.name.compareTo(p2.name);  
-        });  
-        for(Product p:list){  
+        Collections.sort(list,(p1,p2)->{                    
+        return p1.name.compareTo(p2.name);                  
+        });                                              
+        for(Product p:list)
+        {  
             System.out.println(p.id+" "+p.name+" "+p.price);  
         }  
-  
-    }  
+      }  
 }  
---
+++ src/main/java/selfpractise/practise1.java
@@ -0,0 +1,21 @@
+package selfpractise;
+
+public class practise1 
+{
+	
+	
+	
+	
+	
+  public static void main(String[] args)
+ {
+	String str ="helloworld";
+	String str1="world";
+	System.out.println(str1.replace(str1,""));
+	System.out.println(str.contains(str1));  
+	  
+	  
+}
+  
+    
+}
